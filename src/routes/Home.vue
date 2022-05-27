de4f73bf7f09d9f9f860fb6d3bd59c292560a6c2
seadonggyun4 <seadonggyun@gmail.com>
2022-05-27 10:14:50 +0900

    🌱 Add seed file

---
+++ src/routes/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="logo"></div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+.logo{
+  margin: 0 auto;
+  width: 200px;
+  height: 200px;
+  background: url(../assets/logo.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
